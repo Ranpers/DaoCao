26c06284a5dba56474614e387d1f6b73dbe5c346
Yiran <yi_ran@aliyun.com>
2024-01-05 11:42:15 +0800

    配置分页插件

---
+++ daocao_support/src/main/java/pers/yiran/support/config/mybatisplus/MyBatisPlusConfig.java
@@ -0,0 +1,23 @@
+package pers.yiran.support.config.mybatisplus;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("pers.yiran.*.mapper")
+public class MyBatisPlusConfig {
+    /**
+     * 添加分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
+        interceptor.addInnerInterceptor(paginationInnerInterceptor);//如果配置多个插件,切记分页最后添加
+        return interceptor;
+    }
+}
